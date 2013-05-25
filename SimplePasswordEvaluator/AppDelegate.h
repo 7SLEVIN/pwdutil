ceb2c62adb0a1a4575d37c326d78d18028264e52
Markus Færevaag <mafaer@gmail.com>
2013-05-25 13:24:11 +0200

    Initial Commit

---
+++ SimplePasswordEvaluator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimplePasswordEvaluator
+//
+//  Created by Markus Færevaag on 25.05.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SimplePasswordEvaluator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimplePasswordEvaluator
+//
+//  Created by Markus Færevaag on 25.05.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
